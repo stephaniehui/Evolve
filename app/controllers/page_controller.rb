2b9174d3a107801b49c3f6a26afab0ec74d30af7
ramatevish <alexandermatevish@gmail.com>
2013-10-23 17:31:44 -0700

    Added first remedial implementation of admin-defined routes

--- app/controllers/page_controller.rb
+++ app/controllers/page_controller.rb
@@ -2,12 +2,8 @@ class PageController < ActionController::Base
   layout 'application'
 
   def show
-    @page = Page.create :title => "Derpy title",
-                        :content => "Derpy content",
-                        :description => "A page description",
-                        :url => "http://thispage.com",
-                        :content_type => "article"
-    @partials = ['page/partials/page_features', 'page/partials/content']
+    @page = Page.find(params['id'])
+    @partials = []
     render 'page/show'
   end
 

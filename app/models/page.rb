fbfffcf8c7cd6134edba71e7123c973581620a44
Charling Huang <saasbook@saasbook.(none)>
2013-10-27 21:38:48 -0700

    validation of :path, :title, :url added

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,9 @@
 class Page < ActiveRecord::Base
   has_and_belongs_to_many :blocks
 	attr_accessible :path, :title, :published, :content, :content_type, :url, :description
+
+  validates :path, presence: true, uniqueness: true
+
+  validates :title, :url, presence: true, uniqueness: true
+
 end

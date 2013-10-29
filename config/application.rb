cefb87bfe5b9a601d487f286ef8cccee12e3cb6d
ramatevish <alexandermatevish@gmail.com>
2013-10-29 13:25:52 -0700

    Changed application.rb to no longer precompile assets

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,9 @@ module Evolve
     # config.autoload_paths += %W(#{config.root}/extras)
     config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
 
+    # Don't precompile assets
+    config.assets.initialize_on_precompile = false
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]

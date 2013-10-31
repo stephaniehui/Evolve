9534fc24b66627260c9adc8b8fe122b10a1948a9
ramatevish <alexandermatevish@gmail.com>
2013-10-31 15:12:53 -0700

    Pages are only displayed if published.

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,12 @@ Evolve::Application.routes.draw do
   devise_for :users
   resources :users, :page
 
+  # a kludge to work initial page table creation issues
   if ActiveRecord::Base.connection.tables.include?('pages')
     Page.all.each do |page|
-      match page.path, :to => 'page#show', :id => page.id
+      if page.published?
+        match page.path, :to => 'page#show', :id => page.id
+      end
     end
   end
 end

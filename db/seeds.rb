b6595460251f8f4698433623b24d17db209b0ff5
ramatevish <alexandermatevish@gmail.com>
2013-11-07 00:05:27 -0800

    Changed seed.rb to allow for Admin user creation in production

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-if Rails.env.development?
+if Rails.env.development? or Rails.env.production?
   User.find_or_create_by_email :name => ENV['ADMIN_NAME'].dup, :email => ENV['ADMIN_EMAIL'].dup, :password => ENV['ADMIN_PASSWORD'].dup, :password_confirmation => ENV['ADMIN_PASSWORD'].dup
 end
 if Rails.env.test? or Rails.env.development?

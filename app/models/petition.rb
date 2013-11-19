38dcce6d67aef6c6c915c48285fbfbeba0bdb836
ramatevish <alexandermatevish@gmail.com>
2013-11-18 21:42:25 -0800

    Added model dependencies

--- app/models/petition.rb
+++ app/models/petition.rb
@@ -1,5 +1,6 @@
 class Petition < ActiveRecord::Base
   belongs_to :page
+  has_many :supporters, :dependent => :destroy
   attr_accessible :name
   validates_presence_of :name
   validates_uniqueness_of :name

ff3038bceb57e73f64d385a76639b038aaaf4054
ramatevish <alexandermatevish@gmail.com>
2013-11-21 12:47:13 -0800

    Fixed supportable cleanup

--- app/models/supporter.rb
+++ app/models/supporter.rb
@@ -2,5 +2,8 @@ class Supporter < ActiveRecord::Base
   belongs_to :supportable, :polymorphic => true
   attr_accessible :email, :supportable_id, :supportable_type
   validates_with EmailValidator, :attr => :email
-  #TODO: Fix supporter cleanup on page deletion
+
+  def supportable_type=(sType)
+    super(sType.to_s.classify.constantize.base_class.to_s)
+  end
 end

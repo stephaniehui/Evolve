ec2b181aaca76bc1a23bca2be2da30c2e5f86aa1
ramatevish <alexandermatevish@gmail.com>
2013-11-25 21:15:57 -0800

    Recompiled assets after fixing map config

---
+++ public/assets/ckeditor/config-949a7a3631f6a8e3a413adc2afe617f2.js
@@ -0,0 +1,5 @@
+/**
+ * @license Copyright (c) 2003-2013, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.html or http://ckeditor.com/license
+ */
+CKEDITOR.editorConfig=function(t){t.toolbarGroups=[{name:"clipboard",groups:["clipboard","undo"]},{name:"editing",groups:["find","selection","spellchecker"]},{name:"links"},{name:"insert"},{name:"forms"},{name:"tools"},{name:"document",groups:["mode","document","doctools"]},{name:"others"},"/",{name:"basicstyles",groups:["basicstyles","cleanup"]},{name:"paragraph",groups:["list","indent","blocks","align","bidi"]},{name:"styles"},{name:"colors"},{name:"about"}],t.removeButtons="Underline,Subscript,Superscript",t.format_tags="p;h1;h2;h3;pre",t.removeDialogTabs="image:advanced;link:advanced",e.extraPlugins="doksoft_maps"};

2069eb452f458cdb67bba7368d987ead0508d0e4
unknown <sephirothangel@gmail.com>
2013-12-09 16:32:50 -0800

    Fixed bug with openssl

--- app/mailers/supporter_mailer.rb
+++
@@ -1,9 +0,0 @@
-class SupporterMailer < ActionMailer::Base
-  default from: "admin@evolve.com"
-  
-  def self.subscribe(supporter, list_id)
-    @supporter = supporter
-	gb = Gibbon::API.new
-	gb.lists.subscribe({:id => 'list_id', :email => {:email => @supporter.email}, :merge_vars => {:FNAME => @supporter.name_first, :LNAME => @supporter.name_last}, :double_optin => false, :send_welcome => true})
-  end
-end
